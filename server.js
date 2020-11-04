dd90aec06ba08754d2f61e279def9fc9ade1392a
arkady likovizki <arkados2@gmail.com>
2020-11-04 22:25:20 +0200

    test 1.0

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PREFIX = "/api";
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 // app.get("/", (req, res) => {
