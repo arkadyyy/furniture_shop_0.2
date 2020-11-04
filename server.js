2275042afa6a53ab6abcaa2f6eae398dcfae4fb0
arkady likovizki <arkados2@gmail.com>
2020-11-04 20:58:03 +0200

    test 1.0

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const PREFIX = "/api";
 
-app.use("/", express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
